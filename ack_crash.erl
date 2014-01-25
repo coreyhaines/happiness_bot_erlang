e98d8614d1a576f51217e5bc302215934233c7e7
Corey Haines <coreyhaines@gmail.com>
2014-01-25 14:58:17 -0600

    Need to pass SendsTweets back through State

--- ack_crash.erl
+++ ack_crash.erl
@@ -2,6 +2,9 @@
 -behaviour(gen_server).
 
 -export([start_link/3, init/1, handle_call/3, terminate/2]).
+-export([start_default/1]).
+
+start_default(SendsTweets) -> ack_crash:start_link(SendsTweets, "Good Morning", ["Good Day", "Good Night"]).
 
 start_link(SendsTweets, Phrase, Responses) -> gen_server:start_link(?MODULE, [SendsTweets, Phrase, Responses], []).
 
@@ -14,28 +17,10 @@ handle_call({tweet, Twitterer}, From, [SendsTweets, Phrase, []]) ->
 handle_call({tweet, Twitterer}, From, [SendsTweets, Phrase, Responses]) ->
   [Response|Rest] = Responses,
   SendsTweets ! {tweet, Response},
-  {reply, Response, [Phrase, Rest]}.
+  io:format("Received tweet from ~p\nResponses left: ~p", [Twitterer, Rest]),
+  {reply, Response, [SendsTweets, Phrase, Rest]}.
 
 terminate(Reason, State) ->
   io:format("Terminating: ~p", [Reason]).
 
-spread_joy(_, _, []) ->
-  receive
-    {tweet, _} ->
-      {stop, no_more_responses};
-    _ ->
-      {stop, unknown_message}
-  end;
-
-spread_joy(SendsTweets, Phrase, Responses) ->
-  receive
-    {tweet, Twitterer} ->
-      [Response|Rest] = Responses,
-      SendsTweets ! {tweet, Twitterer, Response},
-      spread_joy(SendsTweets, Phrase, Rest);
-    {stop} ->
-      {stop, received_stop};
-    _ ->
-      {stop, unknown_message}
-  end.
 
